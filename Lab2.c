b9a6a9bb33bc5441c17dedeaace0d234cd5c8a02
Nicolas Fajardo <nicolasfajardo@email.arizona.edu>
2014-10-13 13:23:30 -0700

    Kevin KeypadScan function; added c functionality

--- Lab2.c
+++ Lab2.c
@@ -1,88 +1,182 @@
+
 // ******************************************************************************************* //
+//  Kevin Morris
+//  Due: 10/17/14
+
+
+
+
+
+
+
+// File:         lab2.c
+// Date:         08-20-2010
+// Authors:      Roman Lysecky
+// Revised:		 08-27-2013 (R. Thamvichai)
+//
+// Description:  Software code for Lab 0 assignment for ECE 372 Fall 2010. Sample code toggles
+//               a user specified LED on the Microchip 16-bit 28-pin starter board. The user
+//               specified LED is received using the PIC's UART.
+//
+// Requirements: This softwrae code requires the MPLAB C30 Compiler or MPLAB XC16 (MPLAB C Compiler
+//               for PIC24 MCUs)
+//
+// Credits:      Software code based upon sample code provided with Microchip 16-bit 28-pin
+//               Development board.
+//
+// ******************************************************************************************* //
+
 // Include file for PIC24FJ64GA002 microcontroller. This include file defines
 // MACROS for special function registers (SFR) and control bits within those
 // registers.
 
 #include "p24fj64ga002.h"
 #include <stdio.h>
-#include "lcd.h"
 
-// ******************************************************************************************* //
-// Configuration bits for CONFIG1 settings. 
-//
-// Make sure "Configuration Bits set in code." option is checked in MPLAB.
-//
-// These settings are appropriate for debugging the PIC microcontroller. If you need to 
-// program the PIC for standalone operation, change the COE_ON option to COE_OFF.
 
-_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & 
-		 BKBUG_ON & COE_ON & ICS_PGx1 & 
-		 FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768 )
+_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF &
+         BKBUG_ON & COE_ON & ICS_PGx1 &
+         FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768 )
+
 
 // ******************************************************************************************* //
 // Configuration bits for CONFIG2 settings.
 // Make sure "Configuration Bits set in code." option is checked in MPLAB.
+// This option can be set by selecting "Configuration Bits..." under the Configure
+// menu in MPLAB.
 
 _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
-		 IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
+         IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
+
+// ******************************************************************************************* //
+// Defines to simply UART's baud rate generator (BRG) register
+// given the oscillator frequency and PLLMODE.
+
+#define XTFREQ          7372800         	  // On-board Crystal frequency
+#define PLLMODE         4               	  // On-chip PLL setting (Fosc)
+#define FCY             (XTFREQ*PLLMODE)/2    // Instruction Cycle Frequency (Fosc/2)
+
+#define BAUDRATE         115200
+#define BRGVAL          ((FCY/BAUDRATE)/16)-1
+
+
+volatile char KeypadScanReturn = 0;
+volatile int scanKeypad = 0; // a press has been made
+
 
 // ******************************************************************************************* //
 
-// Varible used to indicate that the current configuration of the keypad has been changed,
-// and the KeypadScan() function needs to be called.
+// main function
 
-volatile char scanKeypad;
+// CN detects press
+// CN ISR notes change
+// if change, KeypadScan() investigates the press
+// if(scanKeypad = 1){ KeypadScanReturn = KeypadScan();}
+//
+//
+
+
 
 // ******************************************************************************************* //
 
-int main(void)
-{
-	char key;
-	
-	// TODO: Initialize and configure IOs, LCD (using your code from Lab 1), 
-	// UART (if desired for debugging), and any other configurations that are needed.
-	
-	LCDInitialize();
-	KeypadInitialize();
-	
-	// TODO: Initialize scanKeypad variable.
-	
-	while(1)
-	{
-		// TODO: Once you create the correct keypad driver (Part 1 of the lab assignment), write
-		// the C program that use both the keypad and LCD drivers to implement the 4-digit password system.
-		
-		if( scanKeypad == 1 ) {
-			key = KeypadScan();
-			if( key != -1 ) {
-				LCDMoveCursor(0,0);		
-				LCDPrintChar(key);
-			}
-			scanKeypad = 0;
-		}		
-	}
-	return 0;
+    // Outputs:
+    //      RA0 [LATAbits.LATA0 = O or 1] --> row 1
+    //      RA1 [LATAbits.LATA1 = 0 or 1] --> row 2
+    //      RB2 [LATBbits.LATB2 = 0 or 1] --> row 3
+    //      RB3 [LATBbits.LATB3 = 0 or 1] --> row 4
+
+    // Inputs:
+    //      RB5  [PORTBbits.RB5  == ... ] --> col 1
+    //      RB10 [PORTBbits.RB10 == ... ] --> col 2
+    //      RB10 [PORTBbits.RB11 == ... ] --> col 3
+
+
+char KeypadScan(){
+    key = -1;
+    
+    
+
+    // set Outputs to 0111
+    LATAbits.LATA0 = 0;
+    LATAbits.LATA1 = 1;
+    LATBbits.LATB2 = 1;
+    LATBbits.LATB3 = 1;
+        // if inputs 011, then key = 1
+        // else if inputs 101, then key = 2
+        // else if inputs 110, then key = 3
+    if(PORTBbits & 0x0C20 == 0x0420){
+        key = '1';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0820){
+        key = '2';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0C00){
+        key = '3';
+    }
+        
+    // set outputs to 1011
+    LATAbits.LATA0 = 1;
+    LATAbits.LATA1 = 0;
+    LATBbits.LATB2 = 1;
+    LATBbits.LATB3 = 1;
+        // if inputs 011, then key = 4
+        // else if inputs 101, then key = 5
+        // else if inputs 110, then key = 6
+    if(PORTBbits & 0x0C20 == 0x0420){
+        key = '4';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0820){
+        key = '5';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0C00){
+        key = '6';
+    }
+    
+    // set outputs to 1101
+    LATAbits.LATA0 = 1;
+    LATAbits.LATA1 = 1;
+    LATBbits.LATB2 = 0;
+    LATBbits.LATB3 = 1;
+        // if inputs 011, then key = 7
+        // else if inputs 101, then key = 8
+        // else if inputs 110, then key = 9
+    if(PORTBbits & 0x0C20 == 0x0420){
+        key = '7';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0820){
+        key = '8';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0C00){
+        key = '9';
+    }
+    // set outputs to 1110
+    LATAbits.LATA0 = 1;
+    LATAbits.LATA1 = 1;
+    LATBbits.LATB2 = 1;
+    LATBbits.LATB3 = 0;
+        // if inputs 011, then key = *
+        // else if inputs 101, then key = 0
+        // else if inputs 110, then key = #
+    if(PORTBbits & 0x0C20 == 0x0420){
+        key = '*';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0820){
+        key = '0';
+    }
+    else if(PORTBbits & 0x0C20 == 0x0C00){
+        key = '#';
+    }
+    
+    
+    return key;
 }
 
-// ******************************************************************************************* //
-// Defines an interrupt service routine that will execute whenever any enable
-// input change notifcation is detected.
-// 
-//     In place of _ISR and _ISRFAST, we can directy use __attribute__((interrupt))
-//     to inform the compiler that this function is an interrupt. 
-//
-//     _CNInterrupt is a macro for specifying the interrupt for input change 
-//     notification.
-//
-// The functionality defined in an interrupt should be a minimal as possible
-// to ensure additional interrupts can be processed. 
-void __attribute__((interrupt)) _CNInterrupt(void)
-{	
-	// TODO: Clear interrupt flag
-	IFS1bits.CNIF = 0;
-	
-	// TODO: Detect if *any* key of the keypad is *pressed*, and update scanKeypad
-	// variable to indicate keypad scanning process must be executed.
-}
+void __attribute__((interrupt)) _CNInterrupt(void){
+    
+    scanKeypad = 1;
+    IFS1bits.CNIF = 0;
+    
+    
+    }
 
-// ******************************************************************************************* //
+
