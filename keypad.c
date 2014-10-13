b250461e66d6e42786666d0c712ddf30436a6134
unknown <pcross@email.arizona.edu>
2014-10-13 13:06:55 -0700

    finished keypad initialization was less work than i expected

--- keypad.c
+++ keypad.c
@@ -10,6 +10,39 @@ void KeypadInitialize() {
 	// TODO: Configure IOs and Change Notificaiton interrupt for keypad scanning. This 
 	// configuration should ensure that if any key is pressed, a change notification interrupt 
 	// will be generated.
+
+	// configure outputs
+	TRISAbits.TRISA0=0;
+	TRISAbits.TRISA1=0;
+	TRISBbits.TRISB2=0;
+	TRISBbits.TRISB3=0;
+
+	// configure inputs 
+	TRISBbits.TRISB5=1;
+	TRISBbits.TRISB10=1;
+	TRISBbits.TRISB11=1;
+
+	// configure outputs to open drain
+	ODCAbits.ODA0=1;
+	ODCAbits.ODA1=1;
+	ODCBbits.ODB2=1;
+	ODCBbits.ODB3=1;
+
+	// set ouputs to zero to allow change notification
+	LATAbits.LATA0=0;
+	LATAbits.LATA1=0;
+	LATBbits.LATB2=0;
+	LATBbits.LATB3=0;
+
+	//set internal pull up resistors for inputs 
+	CNPU1bits.CN27PUE = 1;
+	CNPU1bits.CN15PUE = 1;
+	CNPU1bits.CN16PUE = 1;
+
+	// set change interupt flag to zero and enable change ISR
+	IFS1bits.CNIF = 0;
+	IEC1bits.CNIE = 1;
+
 }
 
 // ******************************************************************************************* //
